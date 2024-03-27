75e4fcfd1932c2605e5653061702628068db42ca
Maurice <maurice.nzai@gmail.com>
2024-03-27 21:27:57 +0000

    Update Task 0: first attempt

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -32,11 +32,3 @@ try:
                 i = 0
 except Exception:
     pass
-
-finally:
-    print("File size: {:d}".format(total_file_size))
-    sorted_keys = sorted(status_code.keys())
-    for key in sorted_keys:
-        val = status_code[key]
-        if val != 0:
-            print("{}: {}".format(key, val))
